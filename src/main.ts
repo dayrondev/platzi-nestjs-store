371aba3db3971e08a3ccd6c17653cb5f6760e9a6
dayrondev <dayrondev@gmail.com>
2023-08-14 18:22:06 +0200

    refactoring code and add new features

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
 import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true
     })
   )
-  await app.listen(3000)
+
+  const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('PLATZI STORE')
+    .setVersion('1.0')
+    //.addTag('cats')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  app.enableCors()
+
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap()

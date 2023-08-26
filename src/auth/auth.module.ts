63946f4ee58dc2c5d679ff6c9cdd5b78478bfdaf
dayrondev <dayrondev@gmail.com>
2023-08-27 00:54:45 +0200

    login

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigType } from '@nestjs/config'
+
+import { AuthService } from './services/auth.service'
+import { LocalStrategy } from './strategies/local.strategy'
+import { UsersModule } from '../users/users.module'
+import { AuthController } from './controllers/auth.controller'
+import config from '../../config'
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    /*JwtModule.register({
+      secret: 'this is the key to generate the token',
+      signOptions: {
+        expiresIn: '10d'
+      }
+    })*/
+    JwtModule.registerAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        return {
+          secret: configService.jwtSecret,
+          signOptions: {
+            expiresIn: '10d'
+          }
+        }
+      }
+    })
+  ],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController]
+})
+export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -10,17 +10,28 @@ import { User } from './entities/user.entity'
 import { ProductsModule } from '../products/products.module'
 import { Order } from './entities/order.entity'
 import { OrderProduct } from './entities/order-product.entity'
-import { OrdersService } from './services/orders.service';
-import { OrdersController } from './controllers/orders.controller';
-import { OrderProductController } from './controllers/order-product.controller';
-import { OrderProductService } from './services/order-product.service';
+import { OrdersService } from './services/orders.service'
+import { OrdersController } from './controllers/orders.controller'
+import { OrderProductController } from './controllers/order-product.controller'
+import { OrderProductService } from './services/order-product.service'
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Customer, User, Order, OrderProduct]),
     ProductsModule
   ],
-  controllers: [CustomerController, UsersController, OrdersController, OrderProductController],
-  providers: [CustomersService, UsersService, OrdersService, OrderProductService]
+  controllers: [
+    CustomerController,
+    UsersController,
+    OrdersController,
+    OrderProductController
+  ],
+  providers: [
+    CustomersService,
+    UsersService,
+    OrdersService,
+    OrderProductService
+  ],
+  exports: [UsersService]
 })
 export class UsersModule {}
